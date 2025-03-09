b17aad7a7b15a13762fd58306614e97e11cf3e2c
Avneet Singh Kapoor <avneet.kapoor28@gmail.com>
2025-03-09 17:33:56 +0530

    Added controller and enpoint for getting Youtube feed

--- Backend/Controllers/currentDataControllers.js
+++ Backend/Controllers/currentDataControllers.js
@@ -16,4 +16,28 @@ const getCollisionsCount = async (req, res, next) => {
     }
 }
 
-export default { getCollisionsCount };
+
+const getYoutubeFeed = async (req,res, next) => {
+    try{
+
+        const response = await axios.get(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=formula-1%20news&key=${process.env.YOUTUBE_API_KEY}`);
+        const videoList = extractVideoDetails(response.data);
+        return res.json(videoList);
+
+    }
+    catch(error){
+        console.error('Error fetching data from Youtube API', error);
+        next(error)
+    }
+}
+
+//To extract the video details from the Youtube API response
+const extractVideoDetails = (apiResponse) => {
+    return apiResponse.items.map(item => ({
+        videoId: item.id.videoId,
+        title: item.snippet.title,
+        description: item.snippet.description
+    }));
+};
+
+export default { getCollisionsCount, getYoutubeFeed };
--- Backend/Routes/currentDataRoutes.js
+++ Backend/Routes/currentDataRoutes.js
@@ -4,5 +4,6 @@ import currentData from '../Controllers/currentDataControllers.js';
 const currentDataRouter = express.Router();
 
 currentDataRouter.get('/collisionCount/:year', currentData.getCollisionsCount);
+currentDataRouter.get('/youtubeFeed', currentData.getYoutubeFeed);
 
 export default currentDataRouter;
